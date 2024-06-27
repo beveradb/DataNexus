c251ecbd187dc39784ec008ee4e90fb65990360f
KarliJStroman <159171205+KarliJStroman@users.noreply.github.com>
2024-06-27 13:05:03 -0400

    added input field for testing purposes

--- src/Pages/HomePage/HomePage.js
+++ src/Pages/HomePage/HomePage.js
@@ -131,6 +131,9 @@ function HomePage({ setResultData }) {
             <form>
                 <input type="file" id="fileInput" accept=".xlsx" />
                 <br />
+                <label>Tell us what you want to see</label>
+                <input></input>
+                <br />
                 <pre id="output">{output}</pre>
                 <br />
                 <div id="resultData"></div>
